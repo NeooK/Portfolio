a7e611bcf7c780739ba0ceda7c96e87b2b595d59
Ivan Horyn <NeooK3226@gmail.com>
2022-12-08 19:35:17 +0200

    NEW VERSION

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,9 @@ import {
 } from 'react-icons/ai';
 import Image from 'next/image';
 import deved from '../public/img/dev.png'; 
+import designe from '../public/designe.png';
+import code from '../public/code.png';
+import consulting from '../public/consulting.png';
  
 export default function Home() {
   return (
@@ -60,13 +63,61 @@ export default function Home() {
               I ofer from a wide range of services, including brand designe,
               programming and teaching.
             </p>
-            <div className="">
-              <div className="">
-                
-              </div>
-            </div>
-          </div>
+           <div>
+             <div className="text-center shadow-lg p-10 rounded-xl my-10">
+               <Image src={designe} width={100} height={100} className="display: inline-block"/>
+               <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designe</h3>
+               <p className="py-2">
+                 Creating elegant designs suited for your needs following core designe theory.
+               </p>
+               <h4 className="py-4 text-teal-500">Designe too I use</h4>
+               <p className="tex-gray-800 py-2">Illustrator</p>
+               <p className="tex-gray-800 py-2">Figma</p>
+               <p className="tex-gray-800 py-2">Figma</p>
+             </div>
+             <div className="text-center shadow-lg p-10 rounded-xl my-10">
+               <Image src={code} width={100} height={100} className="display: inline-block"/>
+               <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designe</h3>
+               <p className="py-2">
+                 Creating elegant designs suited for your needs following core designe theory.
+               </p>
+               <h4 className="py-4 text-teal-500">Designe too I use</h4>
+               <p className="tex-gray-800 py-2">Illustrator</p>
+               <p className="tex-gray-800 py-2">Figma</p>
+               <p className="tex-gray-800 py-2">Figma</p>
+             </div>
+             <div className="text-center shadow-lg p-10 rounded-xl my-10">
+               <Image src={consulting} width={100} height={100} className="display: inline-block"/>
+               <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designe</h3>
+               <p className="py-2">
+                 Creating elegant designs suited for your needs following core designe theory.
+               </p>
+               <h4 className="py-4 text-teal-500">Designe too I use</h4>
+               <p className="tex-gray-800 py-2">Illustrator</p>
+               <p className="tex-gray-800 py-2">Figma</p>
+               <p className="tex-gray-800 py-2">Figma</p>
+             </div>
+           </div>
+         </div>
         </section>
+        <section>
+        <div>
+            <h3 className="text-3xl py-1">Servises I offer</h3>
+            <p className="text-md py-2 leanding-8 text-gray-80 dark:text-gray-200">
+              Since the beginning of my jorneyas a freelance designer
+              developer, I've done remote work for 
+              <span className="text-teal-500"> agencies</span>
+              consulted for <span className="text-teal-500"> startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              I ofer from a wide range of services, including brand designe,
+              programming and teaching.
+            </p>
+           </div>
+        </section>
+
       </main>
     </div>
   )
